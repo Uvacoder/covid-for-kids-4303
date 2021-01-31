5108d7cf67c8b4d9e75a6898eaec332e4c81dea1
Neel Redkar <neel.redkar@gmail.com>
2021-01-30 20:04:10 -0800

    add meta descrptions

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import { ThemeProvider } from "theme-ui"
 import theme from "../components/theme"
 import { Flex, Image, Text } from "rebass"
 import Link from "next/link"
+import Head from "next/head"
 
 export default ({ Component, pageProps }) => {
     return (
@@ -11,6 +12,25 @@ export default ({ Component, pageProps }) => {
                 width: "100vw",
                 minHeight: "100vh"
             }}>
+                <Head>
+                    <title>CoviDB 4 Kids</title>
+                    <meta name="title" content="CoviDB 4 Kids" />
+                    <meta name="description" content="CoviDB 4 Kids is a COVID-19 (a new coronavirus) educational resource database." />
+
+
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://kids.covidb.org" />
+                    <meta property="og:title" content="CoviDB 4 Kids" />
+                    <meta property="og:description" content="CoviDB 4 Kids is a COVID-19 (a new coronavirus) educational resource database." />
+                    <meta property="og:image" content="https://kids.covidb.org/static/preview.png" />
+
+                    <meta property="twitter:card" content="summary_large_image" />
+                    <meta property="twitter:url" content="https://kids.covidb.org" />
+                    <meta property="twitter:title" content="CoviDB 4 Kids" />
+                    <meta property="twitter:description" content="CoviDB 4 Kids is a COVID-19 (a new coronavirus) educational resource database." />
+                    <meta property="twitter:image" content="https://kids.covidb.org/static/preview.png" />
+
+                </Head>
                 <Flex width="100vw" height="70px">
                     <Link href="/">
                         <Image sx={{
